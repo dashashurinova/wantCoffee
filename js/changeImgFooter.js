b98a9b2d719bf7dd9b855fc356de53202d36f901
dashashurinova <dasha.shurinova@yandex.ru>
2024-01-22 18:56:04 +0700

    footer buttons

--- js/changeImgFooter.js
+++ js/changeImgFooter.js
@@ -2,14 +2,21 @@
 
 let footerFavsCircle = document.querySelectorAll('.footer__container_first-favs-circle');
 
-
  // twitter
 footerFavsCircle[0].addEventListener('mouseover', function () {
     document.getElementById('twitCont').src="./img/twitter-dark.svg"
 })
+
 footerFavsCircle[0].addEventListener('mouseout', function () {
     document.getElementById('twitCont').src="./img/twitter.svg"
 })
+footerFavsCircle[0].addEventListener('touchEvent', function () {
+    document.getElementById('twitCont').src="./img/twitter.svg"
+})
+
+
+
+
 
  // inst
 footerFavsCircle[1].addEventListener('mouseover', function () {
